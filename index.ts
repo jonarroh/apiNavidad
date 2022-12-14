b5d400f8d487bae12a29f86b540a197b524f77f1
jonarroh <jonarrodi99@gmail.com>
2022-12-14 15:46:28 -0600

    agregado cors

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { Application } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 
 const app = new Application();
+app.use(oakCors());
 
 const randomMessage = () => {
 	// mensajes random de navidad
